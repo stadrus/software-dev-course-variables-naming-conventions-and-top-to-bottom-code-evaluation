24f8693a0e5fdf8a27993a1920f1b7c1cd56eb78
stacey.tadrus@gmail.com <stacey.tadrus@gmail.com>
2025-03-13 13:09:47 -0500

    Updated Variables

---
+++ main.js
@@ -0,0 +1,8 @@
+let userName = "Alice";
+let numberOfItems = 5; 
+const pricePerItem = 4;
+let totalCost = numberOfItems * pricePerItem;
+
+let itemsPurchased = userName + " bought " + numberOfItems + " items for $" + totalCost + ".";
+
+console.log(itemsPurchased);
